bb46415eda6e32a776bb5948aca61122cdbbc3e8
david <djimenez@savispain.es>
2024-12-16 00:51:52 +0100

    383. Ransom Note 2

--- src/Hashmap/easy/ransom_note.py
+++ src/Hashmap/easy/ransom_note.py
@@ -27,21 +27,34 @@ ransomNote and magazine consist of lowercase English letters.
 """
 import math
 import re
+from collections import Counter
 
 import pytest
 
 
 class Solution:
-    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+    def canConstruct(self, ransomnote: str, magazine: str) -> bool:
 
         for x in magazine:
             if ransomnote.find(x) > -1:
                 ransomnote = ransomnote.replace(x, '', 1)
 
         if ransomnote == '':
-            return true
+            return True
         else:
-            return false
+            return False
+
+    def canConstruct2(self, ransomNote, magazine):
+        st1, st2 = Counter(ransomNote), Counter(magazine)
+        # So, basically it will return the difference between the occurrences of each character.
+        # Ex:- suppose,
+        # st1 = Counter({'d': 1, 'e': 1, 'r': 1, 'o': 1})
+        # st2 = Counter({'d': 2, 'i': 2, 'o': 2, 'g': 1, 'e': 1, 'r': 1})
+        # so, st1 & st2 will return Counter({'d': 1, 'e': 1, 'r': 1, 'o': 1})
+        # therefore, if st1 & st2 == st1, will imply that st1 can be formed from st1.
+        if st1 & st2 == st1:
+            return True
+        return False
 
 
 @pytest.mark.parametrize('ransom_note, magazine, expected_output', [
@@ -54,4 +67,7 @@ def test_merge(ransom_note, magazine, expected_output):
     solution = Solution()
     output = solution.canConstruct(ransom_note, magazine)
 
+    assert output == expected_output
+
+    output = solution.canConstruct2(ransom_note, magazine)
     assert output == expected_output
