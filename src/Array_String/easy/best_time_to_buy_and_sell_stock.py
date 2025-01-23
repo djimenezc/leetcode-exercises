3fb175ae5d1642efebdf0d081fd8fdfada941caa
david <djimenez@savispain.es>
2025-01-23 23:40:48 +0100

    best_time_to_buy_and_sell_stock.py

--- src/Array_String/easy/best_time_to_buy_and_sell_stock.py
+++ src/Array_String/easy/best_time_to_buy_and_sell_stock.py
@@ -1,7 +1,8 @@
-'''
+"""
 You are given an array prices where prices[i] is the price of a given stock on the ith day.
 
-You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the
+future to sell that stock.
 
 Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
@@ -24,7 +25,7 @@ Constraints:
 
 1 <= prices.length <= 105
 0 <= prices[i] <= 104
-'''
+"""
 from typing import List
 
 import pytest
@@ -42,6 +43,24 @@ class Solution:
                 max_profit = prices[x] - lowest_price
 
         return max_profit
+
+    # Complexity
+    # Time complexity: O(n)
+    # Space complexity: O(1)
+    def maxProfit(self, prices: List[int]) -> int:
+
+        buy_price = prices[0]
+        profit = 0
+
+        for p in prices[1:]:
+            if buy_price > p:
+                buy_price = p
+
+            profit = max(profit, p - buy_price)
+
+        return profit
+
+
 
 
 @pytest.mark.parametrize('nums, k', [
--- src/Array_String/easy/find_the_index_of_the_first_occurrence_in_a_string.py
+++ src/Array_String/easy/find_the_index_of_the_first_occurrence_in_a_string.py
@@ -1,5 +1,6 @@
 """
-Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack,
+or -1 if needle is not part of haystack.
 
 
 
